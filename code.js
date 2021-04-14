f2ef07a9a4acc60403a15415ca2647b99b1f0a83
tank666 <yuri@zend.pro>
2021-04-14 20:20:24 +0300

    Reset the names of text layers in frames, groups, etc.

--- code.js
+++ code.js
@@ -1,20 +1,28 @@
-let successCount = 0, layerCount = figma.currentPage.selection.length;
-if (layerCount === 0) {
-    figma.closePlugin("Select at least one Text Layer");
-}
+let onlyTextLayer = 0,
+	successCount = 0,
+	layerCount = figma.currentPage.selection.length;
+console.log("Selected:", layerCount);
+if (layerCount === 0) figma.closePlugin("Select at least one layer");
 figma.currentPage.selection.forEach(node => {
-    if (node.type === "TEXT") {
-        node.name = "";
-        successCount++;
-    }
-    else {
-        figma.notify('⚠️ "' + node.name + '" name was not reset. A text layer must be selected.', { timeout: 4000 });
-    }
+	if (node.type === "TEXT") {
+		node.name = "";
+		successCount++;
+		onlyTextLayer++;
+	}
+	else {
+		let nonText = node.findAll(node => node.type === "TEXT");
+		console.log("[" + node.name + "]", "TextNode:", nonText.length);
+		if (nonText.length === 0) figma.notify(node.name + " doesn't contain text layers", { timeout: 4000 });
+		for (var i = 0; i < nonText.length; i++) {
+			nonText[i].name = "";
+			successCount++;
+		}
+	}
 });
-if (layerCount > 1 && successCount > 1) {
-    figma.notify("✅ " + successCount + " text layer names reset", { timeout: 2000 });
+if (onlyTextLayer > 0) console.log("[Separate TextNode]: ", onlyTextLayer);
+if (successCount > 1) {
+	figma.notify("✅ " + successCount + " text layer names reset", { timeout: 2000 });
 }
-else if (layerCount == 1 && successCount == 1) {
-    figma.notify("✅ Text layer name reset", { timeout: 2000 });
-}
+else if (successCount === 1) figma.notify("✅ Text layer name reset", { timeout: 2000 });
+console.log("Total:", successCount, "text layer names reset");
 figma.closePlugin();
